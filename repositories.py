973e62a7561dcd0f5b56e12842b1754351de4c4b
Yaroslav <zabaraaroslav@gmail.com>
2025-03-26 11:27:18 +0200

    Added RegularFileRepo class for file handling (r\w)

---
+++ repositories.py
@@ -0,0 +1,11 @@
+class RegularFileRepo:
+    def __init__(self, filename: str) -> None:
+        self.filename: str = filename
+
+    def get_text(self) -> str:
+        with open(self.filename, "r") as content_file:
+            return content_file.read()
+
+    def save_bytes(self, bytes: bytes) -> None:
+        with open(self.filename, "wb") as content_file:
+            return content_file.write(bytes)
